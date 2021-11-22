8259a540f2dea662bbea0a4fa128900f8bd1c3f3
erklyn <uenesgursel@gmail.com>
2021-11-22 11:41:53 +0300

    Initial Commit

---
+++ haxor-news-app/haxor_news_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  haxor_news_appApp.swift
+//  haxor-news-app
+//
+//  Created by Utku enes Gürsel on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct haxor_news_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
